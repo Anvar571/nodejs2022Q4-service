36af56c9b4ef36eb5e7eb379071fcf7cea609850
Anvar571 <abdurashidovanvar52@gmail.com>
2023-06-04 12:25:54 +0500

    project structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ArtistsModule } from './artists/artists.module';
+import { TracksModule } from './tracks/tracks.module';
+import { AlbumsModule } from './albums/albums.module';
+import { FavoritesModule } from './favorites/favorites.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ArtistsModule, TracksModule, AlbumsModule, FavoritesModule, UsersModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
